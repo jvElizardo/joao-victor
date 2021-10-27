7b32f90ca31a688421dfb0c9ac4ee58606eb8426
jvElizardo <93288757+jvElizardo@users.noreply.github.com>
2021-10-27 17:13:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var sprite;
+function setup() {
+  createCanvas(400,400);
+  sprite=createsprite(200,300,20,20);
+}
+
+function draw() 
+{
+  
+ // sprite.x=mouseX;
+ // sprite.y=mouseY;
+  background(30);
+drawSprites();
+}
+
+
+
+
